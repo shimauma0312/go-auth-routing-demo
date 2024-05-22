8746632248b465f84228176f216888ac7aa444cb
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-05-22 23:15:19 +0900

    fix path

--- pkg/controllers/signinHandler.go
+++ pkg/controllers/signinHandler.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (app *App) signinHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles(tmp.Signin)
+	t, err := template.ParseFiles(tmp.Dir + tmp.Signin)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
