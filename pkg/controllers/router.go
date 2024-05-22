27d80fcd2af2d3c6a6bca85b61cd70d29214fd7c
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-05-22 23:33:13 +0900

    update path name

--- pkg/controllers/router.go
+++ pkg/controllers/router.go
@@ -17,7 +17,8 @@ func (app *App) Get(r *http.Request, name string) (*sessions.Session, error) {
 func (app *App) NewRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+	mux.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
+	mux.HandleFunc("/", app.homeHandler)
 	mux.HandleFunc("/home", app.homeHandler)
 	mux.HandleFunc("/signin", app.signinHandler)
 	mux.HandleFunc("/signgout", app.signoutHandler)
