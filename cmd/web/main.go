9569faff921897afe9977da0e808437914dc6b37
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-05-23 22:09:27 +0900

    add secure statement

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,17 +4,44 @@ import (
 	controller "go-auth-demo/pkg/controllers"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/sessions"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	app := &controller.App{
-		Store: sessions.NewCookieStore([]byte("secret-key")),
+	// .envファイルから環境変数を読み込む
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
+	// 環境変数からセッションキーを取得する
+	secret := os.Getenv("SESSION_KEY")
+	if secret == "" {
+		log.Fatal("SESSION_KEY is required")
+	}
+
+	// セッションキーを使用して新しいクッキーストアを作成する
+	store := sessions.NewCookieStore([]byte(secret))
+	// クッキーストアのオプションを設定する
+	store.Options = &sessions.Options{
+		Path:     "/",      // クッキーのパスを設定する
+		MaxAge:   3600 * 8, // クッキーの有効期限を設定する（ここでは8時間）
+		HttpOnly: true,     // JavaScriptからのアクセスを禁止する
+		Secure:   true,     // HTTPS上でのみクッキーを送信する
+	}
+
+	// 新しいアプリケーションを作成する
+	app := &controller.App{
+		Store: store,
+	}
+
+	// アプリケーションのルーターを作成する
 	router := app.NewRouter()
-	err := http.ListenAndServe(":8080", router)
+	// HTTPサーバーを起動する
+	err = http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	gorm.io/driver/sqlite v1.5.5 // indirect
 	gorm.io/gorm v1.25.10 // indirect
