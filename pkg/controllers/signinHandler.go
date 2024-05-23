44288a64786248034aad79eaeebea1894492ae20
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-05-23 23:12:44 +0900

    add auth statement

--- pkg/controllers/signinHandler.go
+++ pkg/controllers/signinHandler.go
@@ -6,14 +6,42 @@ import (
 	"text/template"
 )
 
+// サインインリクエストを処理
 func (app *App) signinHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles(tmp.Dir + tmp.Signin)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	// GETリクエストの場合、サインインページを表示
+	if r.Method == "GET" {
+		// サインインページのテンプレートを読み込み
+		t, err := template.ParseFiles(tmp.Dir + tmp.Signin)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// テンプレートを実行し、結果をレスポンスに書き出し。
+		err = t.ExecuteTemplate(w, tmp.Signin, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	}
-	err = t.ExecuteTemplate(w, tmp.Signin, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// POSTリクエストの場合、サインイン処理
+	if r.Method == "POST" {
+		// リクエストからフォームデータを解析
+		r.ParseForm()
+		// フォームからユーザー名とパスワードを取得
+		formUsername := r.FormValue("username")
+		formPassword := r.FormValue("password")
+		// データベースからユーザー情報を取得
+		dbUser, err := app.DB.GetUser(formUsername)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// ユーザー一致をcheck
+		if dbUser.UserID == formUsername && dbUser.Password == formPassword {
+			session, _ := app.Get(r, "user")
+			session.Values["id"] = dbUser.UserID
+			session.Save(r, w)
+			http.Redirect(w, r, "/mypage", http.StatusFound)
+		}
 	}
 }
--- pkg/models/userRepository.go
+++ pkg/models/userRepository.go
@@ -16,8 +16,8 @@ func (db *Database) CreateUser(userId string, password string) error {
 	return result.Error
 }
 
-func (db *Database) GetUser(userId string) (string, error) {
+func (db *Database) GetUser(userId string) (User, error) {
 	user := &User{}
 	result := db.Gorm.First(user, "user_id = ?", userId)
-	return user.UserID, result.Error
+	return *user, result.Error
 }
