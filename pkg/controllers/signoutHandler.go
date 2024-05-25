c199629c7375f6cf95bf25e86a6b97a053bf4255
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-05-25 23:58:29 +0900

    chore: Update signoutHandler to clear user session and redirect to homepage

--- pkg/controllers/signoutHandler.go
+++ pkg/controllers/signoutHandler.go
@@ -3,4 +3,8 @@ package controllers
 import "net/http"
 
 func (app *App) signoutHandler(w http.ResponseWriter, r *http.Request) {
+	session, _ := app.Get(r, "user")
+	session.Values["id"] = nil
+	session.Save(r, w)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
