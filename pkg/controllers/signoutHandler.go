8f9e21b109606edf823323e0835aff460143c939
shimauma0312 <shimabara-ryuji@outlook.jp>
2024-05-29 00:29:29 +0900

    fix session remove

--- pkg/controllers/signoutHandler.go
+++ pkg/controllers/signoutHandler.go
@@ -5,6 +5,7 @@ import "net/http"
 func (app *App) signoutHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := app.Get(r, "user")
 	session.Values["id"] = nil
+	session.Options.MaxAge = -1
 	session.Save(r, w)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
